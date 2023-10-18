c992f53db476f77c20d71eee9fb2c2af56e97e28
caiofariaas <caiofarias077@gmail.com>
2023-10-18 14:06:56 -0300

    produtos

--- docs/script.js
+++ docs/script.js
@@ -3,29 +3,63 @@ const catalogo = [{
     nomeProd: "Air max plus Tn' Black and White",
     descProd: "Leve sua atitude ao máximo no Nike Air Max Plus. uma experiência Air que oferece estabilidade e sustentação premium com um amortecimento inacreditável.",
     precoProd: 295.00,
-    img: "./images/Tenis/Tn' 1 Black and white/TN1 BLACK AND WHITE (1).jpg" 
+    img: "./images/Tenis/Tn' 1 Black and white/TN1 BLACK AND WHITE (1).jpg",
+    img2: "./images/Tenis/Tn' 1 Black and white/TN1 BLACK AND WHITE (2).jpg",
+    img3: "./images/Tenis/Tn' 1 Black and white/TN1 BLACK AND WHITE (3).jpg",
+    img4: "./images/Tenis/Tn' 1 Black and white/TN1 BLACK AND WHITE (4).jpg"
 },
 {
     id: 2,
     nomeProd: "Air max plus Tn' Chargers",
     descProd: "bla",
     precoProd: 295.00,
-    img: "./images/Tenis/Tn' 1 Chargers/TN Chargers (5).jpg"
+    img: "./images/Tenis/Tn' 1 Chargers/TN Chargers (5).jpg",
+    img2: "./images/Tenis/Tn' 1 Chargers/TN Chargers (5).jpg",
+    img3: "./images/Tenis/Tn' 1 Chargers/TN Chargers (4).jpg",
+    img4: "./images/Tenis/Tn' 1 Chargers/TN Chargers (3).jpg"
 },
 {
     id: 3,
     nomeProd: "Air max 95 'NEON' ",
     descProd: "X",
     precoProd: 330.00,
-    img: "./images/Tenis/Air max 95 Neon/Air max 95 Neon (1).jpg"
+    img: "./images/Tenis/Air max 95 Neon/Air max 95 Neon (1).jpg",
+    img2: "./images/Tenis/Air max 95 Neon/Air max 95 Neon (1).jpg",
+    img3: "./images/Tenis/Air max 95 Neon/Air max 95 Neon (2).jpg",
+    img4: "./images/Tenis/Air max 95 Neon/Air max 95 Neon (3).jpg"
 },
 {
     id: 4,
     nomeProd: "Nike shox TL' Black",
     descProd: "X",
     precoProd: 330.00,
-    img: "./images/Tenis/Nike Shox TL Black/Nike Shox TL Black (2).jpg"
-},]
+    img: "./images/Tenis/Nike Shox TL Black/Nike Shox TL Black (2).jpg",
+    img2: "./images/Tenis/Nike Shox TL Black/Nike Shox TL Black (2).jpg",
+    img3: "./images/Tenis/Nike Shox TL Black/Nike Shox TL Black (1).jpg",
+    img4: "./images/Tenis/Nike Shox TL Black/Nike Shox TL Black (3).jpg"
+},
+{
+    id: 5,
+    nomeProd: "Air force 1 '07",
+    descProd: "X",
+    precoProd: 365.00,
+    img: "./images/Tenis/Air force 1 '07/Air force 1 07 (3).jpg", 
+    img2: "./images/Tenis/Air force 1 '07/Air force 1 07 (3).jpg",
+    img3: "./images/Tenis/Air force 1 '07/Air force 1 07 (2).jpg",
+    img4: "./images/Tenis/Air force 1 '07/Air force 1 07 (1).jpg"
+},
+{
+    id: 6,
+    nomeProd: "Air max plus Tn' Black and White",
+    descProd: "X",
+    precoProd: 295.00,
+    img: "./images/Tenis/Tn' 1 White and Black/Tn'1 White and Black (1).jpg", 
+    img2: "./images/Tenis/Tn' 1 White and Black/Tn'1 White and Black (1).jpg",
+    img3: "./images/Tenis/Tn' 1 White and Black/Tn'1 White and Black (2).jpg",
+    img4: "./images/Tenis/Tn' 1 White and Black/Tn'1 White and Black (3).jpg"
+}
+
+]
 
 // Pagina de produtos
 
